a1c74f92d82cda353aec2283edb30e20328a8cb8
lilwebsite <carl@bigwebsite.cool>
2021-03-07 18:17:41 -0500

    globals.h now only includes headers that are needed by most files

--- kbd/globals.h
+++ kbd/globals.h
@@ -4,8 +4,12 @@
 #include <util/setbaud.h>
 #include <util/delay.h>
 #include "types.h"
+#include <promicro/pinlogic.h>
 //define no-operation
 #define _NOP() do { __asm__ __volatile__ ("nop"); } while (0)
 //convenience macros
 #define LOW 0
 #define HIGH 1
+
+void init(void);
+void set(uint8_t row);
