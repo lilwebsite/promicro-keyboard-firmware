c7d27e5b68d0dadd477441c3aeb533d7a48d6ed0
lilwebsite <carl@bigwebsite.cool>
2022-06-25 14:12:07 -0400

    fixed ITT code for both numpad and keyboard

--- drivers/protocol/ITT.c
+++ drivers/protocol/ITT.c
@@ -14,11 +14,11 @@ void scan(void)
 		case 0b0:
 			if(counter == 0)
 			{
-				if(last_pressed_count != pressed_counter || !pressed_counter)
-				{
+//				if(last_pressed_count != pressed_counter || !pressed_counter)
+//				{
 					overrides();
 					kbsend();
-				}
+//				}
 				last_pressed_count = pressed_counter;
 				pressed_counter = 0;
 			}
@@ -91,13 +91,6 @@ void scan(void)
 
 	reset_sending();
 
-	if(received[SOLENOID_DISABLE])
-	{
-		//solenoid_toggle();
-		solenoid &= 0b1;
-		solenoid ^= 0b1;
-	}
-
 	for(; counter < COLUMNS; counter++)
 	{
 		struct keypress target = {keymap[counter], counter, counter};
@@ -106,12 +99,19 @@ void scan(void)
 	counter = 0;
 	
 	#if NO_SOLENOID==0
+	if(received[SOLENOID_DISABLE])
+	{
+		solenoid ^= 0b1;
+		solenoid |= 0b100;
+		_delay_ms(50); // debounce
+	}
+
 	// if a keypress was detected
 	if((clock_data & 0x40) == 0x40)
-	{solenoid = 0b11;}
+	{solenoid = (solenoid & 0b1) | 0b10;}
 
 	if((solenoid & 0b111) == 0b11
-	&& pressed_counter >= (last_pressed_count + 1))
+	&& pressed_counter > last_pressed_count)
 	{
 		// I am using the function call overhead as a delay here
 		// gives it just the right amount of time
